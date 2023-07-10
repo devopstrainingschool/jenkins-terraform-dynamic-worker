6b476ca6af0f3d0c243ca9ad0757e86e95a0708c
Your Name <yannickparker84@gmail.com>
2023-07-10 01:02:39 -0500

    sksk

--- ansible_host.tf
+++ ansible_host.tf
@@ -15,7 +15,7 @@ resource "ansible_host" "REDIS001" {
   vars = {
       ansible_user = "centos"
       role = "master"
-      ansible_ssh_common_args= " -o ProxyCommand=\"ssh -o StrictHostKeyChecking=no -i anael.pem -W %h:%p -q centos@${aws_instance.bastion.public_dns}\""
+      ansible_ssh_common_args= " -o ProxyCommand=\"ssh -o StrictHostKeyChecking=no -i anael.pem -W %h:%p -q ubuntu@${aws_instance.bastion.public_dns}\""
       ansible_ssh_private_key_file="anael.pem"
       ansible_python_interpreter="/usr/bin/python3"
       proxy = "${aws_instance.bastion.private_ip}"
