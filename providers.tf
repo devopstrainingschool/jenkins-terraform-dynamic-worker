5aa2d69fe248dccb29ba0ac1f39247269360ca85
Your Name <yannickparker84@gmail.com>
2023-07-10 08:28:44 -0500

    ksks

--- providers.tf
+++ providers.tf
@@ -1,16 +1,16 @@
+
+provider "aws" {
+    region  = "us-east-2"
+}
 terraform {
   required_providers {
     ansible = {
       source = "nbering/ansible"
       version = "1.0.4"
     }
-    
     aws = {
       source = "hashicorp/aws"
-      version = "~> 4.54.0"
+      version = "~> 4.0.0"
     }
   }
-}
-provider "aws" {
-    region  = "us-east-2"
 }
