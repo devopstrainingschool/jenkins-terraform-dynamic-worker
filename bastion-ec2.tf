2b6b1ff82d2f942fb3d73451510a1d6d404244fb
Your Name <yannickparker84@gmail.com>
2023-07-09 23:10:19 -0500

    sksk

--- bastion-ec2.tf
+++ bastion-ec2.tf
@@ -2,8 +2,7 @@ resource "aws_instance" "bastion" {
     ami = "ami-05a36e1502605b4aa"
     instance_type = "t2.micro"
     subnet_id = "${aws_subnet.public-bastion.id}"
-    #key_name = aws_key_pair.anael.key_name
-    key_name = "anael1"
+    key_name = aws_key_pair.anael.key_name
     associate_public_ip_address = "true"
     vpc_security_group_ids = ["${aws_security_group.bastion.id}"]
     provisioner "file" {
