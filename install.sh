fb345d1c4a4be885e70d565e1617ef404196be64
devopstrainingschool <107158398+devopstrainingschool@users.noreply.github.com>
2023-07-19 19:03:08 -0500

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-sudo yum install java-11-openjdk –y
+sudo yum install java-11-openjdk -y
 sudo yum install wget -y
-sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io-2023.key
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+rpm --import https://pkg.jenkins.io/redhat/jenkins.io-2023.key
 sudo yum upgrade -y
 sudo yum install jenkins -y
 sudo systemctl daemon-reload
